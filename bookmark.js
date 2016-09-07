416367ad0eba48185d6a958bee0ca5703d62948a
Thomas DiG <thomas@devingfx.com>
2016-09-07 19:44:05 +0200

    pffff

---
+++ bookmark.js
@@ -0,0 +1,3 @@
+var win = window.open(null,'LBCDataStore','menubar=no,location=no,resizable=yes,scrollbars=no,status=no'); 
+win.parentWindow = window;
+win.document.write('<script src="https://devingfx.github.io/Miaow/DataStore.js"></script>');
