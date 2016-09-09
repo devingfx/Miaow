22254b70bc38e240ea4b0ae64c8fc88d1510bee8
Thomas DiG <thomas@devingfx.com>
2016-09-09 16:00:43 +0200

    mimimimew

--- DataStore.js
+++ DataStore.js
@@ -141,10 +141,10 @@ window.Store = class Store {
                 this.$content = this.find('.content');
                 this.$footer = this.find('footer');
             }
+            get icon()     { return this.$header.find('> icon') }
+            set icon( v )  { this.$header.find('h1').append(v) }
             get title()     { return this.$header.find('h1').text() }
             set title( v )  { this.$header.find('h1').append(v) }
-            get title2()    { return this.$header.find('span').text() }
-            set title2( v ) { this.$header.find('span').append(v) }
             get head()      { return this.$header }
             set head( v )   { this.$header.append(v) }
             get content()   { return this.$content }
@@ -155,36 +155,37 @@ window.Store = class Store {
     }
     static get Page()
     {
-        return class Page extends Store.Element {
+        return class Page extends Store.Window {
             constructor()
             {
                 // debugger;
-                super('<window>');
+                super('<window page>');
                 // this.$el = $('<section>');
                 // this.$el[0].ctrl = this;
                 // this.$el[0].innerHTML = `
-                this.html(`
-                    <header>
-                        <h1></h1>
-                        <span></span>
-                    </header>
-                    <div class="content"></div>
-                    <footer></footer>
-                `);
-                this.$header = this.find('header');
-                this.$content = this.find('.content');
-                this.$footer = this.find('footer');
+                this.head = '<span>';
+                // this.html(`
+                //     <header>
+                //         <h1></h1>
+                //         <span></span>
+                //     </header>
+                //     <div class="content"></div>
+                //     <footer></footer>
+                // `);
+                // this.$header = this.find('header');
+                // this.$content = this.find('.content');
+                // this.$footer = this.find('footer');
             }
-            get title()     { return this.$header.find('h1').text() }
-            set title( v )  { this.$header.find('h1').append(v) }
+            // get title()     { return this.$header.find('h1').text() }
+            // set title( v )  { this.$header.find('h1').append(v) }
             get title2()    { return this.$header.find('span').text() }
             set title2( v ) { this.$header.find('span').append(v) }
-            get head()      { return this.$header }
-            set head( v )   { this.$header.append(v) }
-            get content()   { return this.$content }
-            set content( v ){ this.$content.append(v) }
-            get footer()    { return this.$footer }
-            set footer( v ) { this.$footer.append(v) }
+            // get head()      { return this.$header }
+            // set head( v )   { this.$header.append(v) }
+            // get content()   { return this.$content }
+            // set content( v ){ this.$content.append(v) }
+            // get footer()    { return this.$footer }
+            // set footer( v ) { this.$footer.append(v) }
         }
     }
     static get Editor()
