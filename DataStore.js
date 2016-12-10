dd958df6bf3f7df11618d3bccf460d7afc0f8dc8
Thomas DiG <thomas@devingfx.com>
2016-12-10 22:52:29 +0100

    moo

--- DataStore.js
+++ DataStore.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', e=> console.log(e) );
+document.addEventListener('loaded', e=> console.log(e) );
+window.addEventListener('loaded', e=> console.log(e) );
+
 var org;
 function loadSchemaOrg()
 {
@@ -155,10 +159,47 @@ window.SchemaExtractor.schema = Symbol`schema`;
 
 
 var cat = {};
+Symbol.proxified = Symbol`[[proxified]]`;
+Intrication = class Intrication {
+	constructor()
+	{
+		this[Symbol.proxified] = Array.from( arguments );
+		let proxy = new Proxy( this, {
+			get: function( o, k )
+			{
+				if( Reflect.has(o, k) )
+					return Reflect.get( o, k );
+				debugger;
+				for( var obj of o[Symbol.proxified] )
+				{
+					if( Reflect.has(obj, k) )
+					{
+						let desc = (cur => {
+										var desc;
+										while( cur.__proto__ != Object )
+										{
+											if( desc = Object.getOwnPropertyDescriptor( cur, k ) ) break;
+											cur = cur.__proto__;
+										}
+										return desc;
+									})( obj );
+						return desc
+								? desc.get && desc.get.bind( /native code/.test(desc.get.toString()) ? obj : proxy )()
+						 	 		|| desc.value
+								: typeof Reflect.get(obj, k) == 'function'
+									? Reflect.get(obj, k).bind(obj)
+									: Reflect.get(obj, k)
+					}
+				}
+			}
+		});
+		return proxy;
+	}
+}
 cat.Element = class Element {
 	constructor( tag )
 	{
-		this._target = $( tag );
+		this[Symbol.proxified] = $( tag );
 		let proxy = new Proxy( this, {
 			get: (o,k) => Reflect.has(this._target,k)
 							? typeof Reflect.get(this._target, k) == 'function'
