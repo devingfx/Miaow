d8212c47933de72e42347c1ded5d039e7b7e5cd7
Thomas DiG <thomas@devingfx.com>
2016-12-21 23:54:56 +0100

    promouuushh

--- views/Store.js
+++ views/Store.js
@@ -37,11 +37,9 @@ org.schema.search('fuel')
 import 'https://cdn.datatables.net/1.10.12/css/jquery.dataTables.css';
 import 'https://devingfx.github.io/Miaow/layout.css';
 
-import { LokiIndexedAdapter, loki } from 'https://devingfx.github.io/Miaow/db.minou.js';
-import lokimodule from 'https://devingfx.github.io/Miaow/db.minou.js';
-console.log(LokiIndexedAdapter);
-console.log(loki);
-console.log(lokimodule);
+// import { LokiIndexedAdapter, loki } from 'https://devingfx.github.io/Miaow/db.minou.js';
+import loki from 'https://devingfx.github.io/Miaow/db.minou.js';
+console.log(window.ikol=loki);
 import 'https://devingfx.github.io/Miaow/db-indexed-adapter.minou.js';
 
 import 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js';
